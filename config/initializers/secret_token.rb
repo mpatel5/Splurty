f647211144bfd807be9e35e8b300c85318ccf380
Mitesh Patel <mpatel5@icloud.comgit config --global user.email mpatel5@icloud.comgit config --global user.email mpatel5@icloud.comgit config --global user.name Mitesh>
2015-12-29 02:36:04 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Splurty::Application.config.secret_key_base = '9fbc81fb424482f1cd8950d1f0064f04d16be55b1401b1b82c7719c61aba24cc33085d4f47dd86ad72944c90c4e4cddbdc1a154bea70f644ef3fae3293802004'
